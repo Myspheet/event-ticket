2a5af9724e4ad654bc406bb638c5919e83b889ac
Promise Oghenevwefe <myspheet@gmail.com>
2026-03-02 01:45:22 +0100

    Added env for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  console.log("Loaded environment variables:", env);
   return {
     plugins: [react()],
     server: {
-      port: 5173,
+      // port: 5173,
       proxy: {
         "/api": {
-          target: env.VITE_API_URL || "http://localhost:4000",
+          target: env.VITE_API_URL,
           changeOrigin: true,
         },
       },
